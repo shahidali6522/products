1581cb238bf29c6e38a60fb5aaa27b3bc68fad90
Shahid Ali <shahidali.saa98@gmail.com>
2023-06-21 16:12:56 +0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,38 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameModule } from './name/name.module';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { HomeComponent } from './home/home.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app-routing.module';
 import { MatInputModule } from '@angular/material/input';
 import { Comp1Component } from './comp1/comp1.component';
 import { MatTableModule } from '@angular/material/table';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
 import { ProductComponent } from './product/product.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, Comp1Component, ProductComponent],
   imports: [
+    NameModule,
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    NameModule,
     ReactiveFormsModule,
-    FormsModule,
 
     // material
-    MatDialogModule,
-    MatButtonModule,
     MatIconModule,
-    MatDividerModule,
     MatInputModule,
     MatTableModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDividerModule,
     MatSlideToggleModule,
   ],
   providers: [],
